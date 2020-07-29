4d7b33ee3cc25aaad77e5da6a77dfe02a1c916b6
ZPinjun <m18846448029@163.com>
2020-07-29 16:33:19 +0800

    Test the first search floor.

--- Chebyshev.m
+++ Chebyshev.m
@@ -38,7 +38,7 @@ for i = 0:T-1
 %     disp(['第',num2str(i+1),'次方向：',num2str((gc*(fc+beta^2*(f(z)-fc + beta*f1c*gc*fc   ))).')]);
     c = c - gc*(fc+beta^2*(f(z)-fc + beta*f1c*gc*fc   ));
     err(i+1) = (2*K)\norm(f(c),2)^2;
-    disp(['第',num2str(i+1),'误差：',num2str(err(i+1))]);
+    disp(['第',num2str(i+1),'次迭代误差：',num2str(err(i+1))]);
     figure(10)
     plot(0:i,err(1:i+1));
 end

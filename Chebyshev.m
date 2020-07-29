b62ce2609faef702d68f1bdd04a7c65bed41b4d2
ZPinjun <m18846448029@163.com>
2020-07-29 15:04:34 +0800

    Finished the second floor.

--- Chebyshev.m
+++ Chebyshev.m
@@ -9,9 +9,11 @@ xi(1) = sqrt(xi(1));
 
 %% --------------------------------------------------- 初始化
 threshold = 10e-4;
-T = 20;
+T = 12;
 beta = 1;
-c = rand(K,1);
+es = (sum(y))/(K*N*norm(h,2)^2);
+c0 = es*ones(K,1);
+% load('c0.mat');
 
 n = (1:N-1).';
 mcomb = nchoosek(m,2);
@@ -25,14 +27,18 @@ s2 = kron(n,ones(K,1))*ones(1,K);
 term2 = cos((2*pi/N)*(s1).*(s2));
 
 err = zeros(T,1);
+c = c0;
 %% --------------------------------------------------- 迭代
 for i = 0:T-1
     fc = f(c);
     f1c = [ones(1,K);2*kron(eye(N-1),c.')*term2];
     gc = (f1c.'*f1c)\f1c.';
     z = c - beta*gc*fc;
+%     disp(['第',num2str(i+1),'次幅度：',num2str(c.')]);
+%     disp(['第',num2str(i+1),'次方向：',num2str((gc*(fc+beta^2*(f(z)-fc + beta*f1c*gc*fc   ))).')]);
     c = c - gc*(fc+beta^2*(f(z)-fc + beta*f1c*gc*fc   ));
     err(i+1) = (2*K)\norm(f(c),2)^2;
+    disp(['第',num2str(i+1),'误差：',num2str(err(i+1))]);
     figure(10)
     plot(0:i,err(1:i+1));
 end
